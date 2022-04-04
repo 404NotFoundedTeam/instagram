f0dc772ec49b813bdcb69e9846bad88baf7cf335
AliwerDev <alwerdev@gmail.com>
2022-04-04 15:18:33 +0500

    asdfasd

--- pages/messenger/index.js
+++ pages/messenger/index.js
@@ -1,8 +1,14 @@
-import { Box, Paper } from "@mui/material";
+import { Box, Paper, Typography } from "@mui/material";
 import React from "react";
+import { useSelector } from "react-redux";
 import Header from "../../components/header";
 
 const Chat = () => {
+  const [messages, setMessages] = React.useState([]);
+  const [message, setMessage] = React.useState("");
+  const [user, setUser] = React.useState("");
+  const users = useSelector((state) => state.users.followers);
+
   return (
     <>
       <Header />
@@ -21,6 +27,9 @@ const Chat = () => {
             sx={{
               height: "500px",
               width: "300px",
+              display: 'flex',
+              flexDirection: 'column',
+
               borderRight: (theme) => theme.customBorders.input,
             }}
           >
@@ -28,10 +37,39 @@ const Chat = () => {
               sx={{
                 width: "100%",
                 borderBottom: (theme) => theme.customBorders.input,
+                display: "flex",
+                padding: "10px",
+                justifyContent: "center",
               }}
-            ></Box>
+            >
+              Aliwerdev
+            </Box>
+            <Box sx={{ width: "100%", overflow: "auto", flex: 1, p: 1 }}>
+              {users.map((user) => (
+                <Box
+                  key={user.id}
+                  sx={{
+                    display: "flex",
+                    padding: "10px",
+                    alignItems: "center",
+                    gap: "5px",
+                    width: "100%",
+                  }}
+                >
+                  <img
+                    src={user.profile_pic || ""}
+                    alt=""
+                    style={{
+                      width: "50px",
+                      height: "5cd0px",
+                      borderRadius: "50%",
+                    }}
+                  />
+                  <Typography>{user.firstname}</Typography>
+                </Box>
+              ))}
+            </Box>
           </Box>
-          <Box></Box>
         </Paper>
       </Box>
     </>
--- redux/reducers/index.js
+++ redux/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import editorReducer from "./editor";
+import usersReducer from "./users";
 
 const rootReducer = combineReducers({
   editor: editorReducer,
+  users: usersReducer,
 });
 
 export default rootReducer;
