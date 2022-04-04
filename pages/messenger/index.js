26bb494bc6067b0523376e37c7269795be63e176
AliwerDev <alwerdev@gmail.com>
2022-04-04 14:24:01 +0500

    fasdf

--- pages/messenger/index.js
+++ pages/messenger/index.js
@@ -6,18 +6,31 @@ const Chat = () => {
   return (
     <>
       <Header />
-      <Box sx={{ width: "100%", height: "100vh", padding: { xs: 0, md: 3 } }}>
+      <Box sx={{ width: "100%", padding: { xs: 0, md: 3 } }}>
         <Paper
           sx={{
             borderRadius: { xs: 0, md: 1 },
             boxShadow: (theme) => ({ md: theme.customShadows.card, xs: 0 }),
             haight: "100%",
             width: "100%",
-            maxWidth: "1200px",
+            maxWidth: "991px",
             mx: "auto",
           }}
         >
-          <Box sx={{height: '200px'}}></Box>
+          <Box
+            sx={{
+              height: "500px",
+              width: "300px",
+              borderRight: (theme) => theme.customBorders.input,
+            }}
+          >
+            <Box
+              sx={{
+                width: "100%",
+                borderBottom: (theme) => theme.customBorders.input,
+              }}
+            ></Box>
+          </Box>
           <Box></Box>
         </Paper>
       </Box>
