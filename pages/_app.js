c1e5e798ba0adce6ba74a9a3ff80b332205634db
AliwerDev <alwerdev@gmail.com>
2022-04-04 10:52:49 +0500

    logo

--- pages/_app.js
+++ pages/_app.js
@@ -3,11 +3,14 @@ import { Provider } from "react-redux";
 import { store } from "../redux/store";
 import { ThemeProvider } from "@mui/material";
 import { theme } from "../theme";
+import MyHead from "../containers/MyHead";
+
 
 function MyApp({ Component, pageProps }) {
   return (
     <Provider store={store}>
       <ThemeProvider theme={theme}>
+        <MyHead />
         <Component {...pageProps} />
       </ThemeProvider>
     </Provider>
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,10 @@
-import Head from "next/head";
+import { Box } from "@mui/material";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Head>
-        <title>Instagram</title>
-        <meta name="description" content="Clone instagram.com" />
-        <link rel="icon" href="/logo_full.png" />
-      </Head>
+
     </div>
   );
 }
